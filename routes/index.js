f4fee6cad8860a490a7147393f498c59ab0af489
SpvaigPvtLtd <info@spvaig.in>
2021-06-29 13:15:18 +0530

    a

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,15 @@ router.get('/', function(req, res, next) {
 
 
 
+router.post('/change-password',(req,res)=>{
+  pool.query(`update ${req.body.type} set password = '${req.body.password}' where id = '${req.body.id}'`,(err,result)=>{
+    if(err) throw err;
+    else res.json({
+      msg : success
+    })
+  })
+})
+
+
+
 module.exports = router;
